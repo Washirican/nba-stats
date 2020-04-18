91f2c30a06e193e2edd65a9f2790c1847ede05ff
Washirican <danrod@uw.edu>
2020-04-17 19:26:28 -0700

    Requesting boxscore data from NBA Stats website

---
+++ nba_shotcharts.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2020-04-17
+# --------------------------------------------------------------------------- #
+
+# Get NBA shot chart for player
+# TODO D. Rodriguez 2020-04-17: Make code run from command line (PowerShell)
+# TODO D. Rodriguez 2020-04-17: Figure out NBA Stats website API endpoints
+# TODO D. Rodriguez 2020-04-17: Make request to NBA Stats website
+# TODO D. Rodriguez 2020-04-17: Process data to extract missed/made shots
+# TODO D. Rodriguez 2020-04-17: Generate (or use external image?)
+#  a court image in plot
+# TODO D. Rodriguez 2020-04-17: Plot shots in charts (or over external image?)
+
+
+def request_data():
+    print('Getting data from NBA Stats Website...')
+
+
+if __name__ == '__main__':
+    print('Hello NBA World!')
+    request_data()
