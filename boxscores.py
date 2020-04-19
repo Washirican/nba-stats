2e5ee2c7dfae9549d1470ab1991f5b267bda9edd
Washirican <danrod@uw.edu>
2020-04-19 12:18:53 -0700

    Requesting game Box Score data from NBA Stats website

--- boxscores.py
+++ boxscores.py
@@ -2,9 +2,18 @@
 # D. Rodriguez, 2020-04-17
 # --------------------------------------------------------------------------- #
 
-# TODO D. Rodriguez 2020-04-17: Get boxscore data for a given game
+# TODO D. Rodriguez 2020-04-17: Get box score data for a given game
 
 import requests
 
+
+def get_box_score():
+    # request_url = 'https://stats.nba.com/game/0021900973/'
+    request_url = 'https://stats.nba.com/stats/boxscoresummaryv2?GameID=0021900973'
+    return requests.get(request_url)
+
+
 if __name__ == '__main__':
-    print('NBA Boxscore')
+    print('NBA Box Score')
+    box_score_data = get_box_score()
+    print(box_score_data)
