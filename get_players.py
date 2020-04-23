7aad3a8f18d47bfc29f9a67fd1b247933444189c
Washirican <danrod@uw.edu>
2020-04-22 21:50:52 -0700

    Get player and team data.

--- get_players.py
+++ get_players.py
@@ -2,22 +2,40 @@
 # D. Rodriguez, 2020-04-22
 # --------------------------------------------------------------------------- #
 
+import json
 import requests
-from bs4 import BeautifulSoup as BS
 
 # TODO D. Rodriguez 2020-04-22: Scrape NBA Stats Team Index for player
 #  names and IDs
 
-player_index_url = 'https://stats.nba.com/players/list/?Historic=Y'
-page = requests.get(player_index_url)
+player_index_url = 'https://stats.nba.com/js/data/ptsd/stats_ptsd.js'
+player_list = requests.get(player_index_url)
 
-soup = BS(page.content, 'html.parser')
+# Cleanup string
+dict_str = player_list.content.decode()[17:-1]
 
-players_list = soup.find_all('li', attrs={'class': 'players-list__name'})
+# Turns string into dictionary
+data = json.loads(dict_str)
+players = data['data']['players']
+teams = data['data']['teams']
+data_date = data['generated']
 
-# players_list = soup.find_all(class_='players-list__name')
+first_name = 'Kobe'
+last_name = 'Bryant'
 
-# players = []
-# for player in players_list.find_all('li', attrs={'class': 'players-list__name'}):
-#     players.append(player)
-    # players['url'] = a['href']
+for player in players:
+    # print(player[1])
+    if f'{last_name.title()}, {first_name.title()}' in player[1]:
+        print(player[:])
+        team_id = player[5]
+        # break
+    # else:
+    # print(f'Player {first_name.title()} {last_name.title()} not found in database.')
+
+for team in teams:
+    if str(team_id) in team[0]:
+        print(team[:5])
+
+# TODO (D. Rodriguez, 2020-04-22): Save player and team info to database
+#  (sqlite3/MongoDB)
+
